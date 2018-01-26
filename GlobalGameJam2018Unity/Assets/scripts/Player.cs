5d07bebe3736cf1073b668b6e2debe9a5ff5f387
tanjaU29798 <u29798@hs-harz.de>
2018-01-26 20:32:40 +0100

    littleChanges

--- GlobalGameJam2018Unity/Assets/scripts/Player.cs
+++ GlobalGameJam2018Unity/Assets/scripts/Player.cs
@@ -67,8 +67,11 @@ public class Player : MonoBehaviour {
 	void Jump(){
 		if (Input.GetButtonDown ("Jump") && jumpB == true) {
             //Vector2 movement = new Vector2 (0.0f, jumpM);
+            float mv = Input.GetAxis("Horizontal");
+            Vector2 movement = new Vector2(mv, jumpM);
+            rb.AddForce(movement * speed);
             //rb.AddForce (movement, ForceMode2D.Impulse);
-            rb.AddForce(Vector2.up * jumpM);
+            //rb.AddForce(Vector2.up * jumpM);
             return;
 		}
         
