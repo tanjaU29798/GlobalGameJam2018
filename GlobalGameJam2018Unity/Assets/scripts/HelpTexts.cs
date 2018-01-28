d140ac8c44be3b692a47a4c87795b02b031d17ed
tanjaU29798 <u29798@hs-harz.de>
2018-01-28 02:04:23 +0100

    menu

---
+++ GlobalGameJam2018Unity/Assets/scripts/HelpTexts.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpTexts : MonoBehaviour {
+
+    [SerializeField]
+    private int counter = 1000;
+    private bool pressed6 = false;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("6")) pressed6 = true;
+
+        if (!pressed6)
+        {
+            if (counter <= 0)
+            {
+                gameObject.GetComponent<Renderer>().enabled = true;
+            }
+            else counter--;
+        }
+        
+	}
+}
